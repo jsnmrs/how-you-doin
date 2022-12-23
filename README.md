1b2ca4f71ce855373320ce5f597f871ff499be6a
Jason Morris <jasonmorris@fastmail.com>
2022-12-22 21:35:49 -0500

    :school_satchel: :books:

---
+++ README.md
@@ -0,0 +1,13 @@
+# Grades
+
+A dashboard of performance, accessibility, SEO, and best practice measurements over time. Powered with [Speedlify](https://github.com/zachleat/speedlify/) by [@zachleat](https://www.zachleat.com/) and GitHub Actions.
+
+<https://jsnmrs.github.io/grades>
+
+## Run locally
+
+```
+npm install
+npm run test-pages
+npm run start
+```
---
+++ _data/sites/projects.js
@@ -0,0 +1,24 @@
+module.exports = {
+	name: "projects", // optional, falls back to object key
+	description: "Personal web projects",
+	skip: false,
+	options: {
+		frequency: 60 * 23, // 23 hours
+		freshChrome: "run"
+	},
+	urls: [
+		"https://jasonmorris.com/",
+		"https://jasonmorris.com/resume/",
+		"https://jasonmorris.com/bikes/bikes-and-barns/",
+		"https://jasonmorris.com/bikes/where-do-you-see-yourself-in-20-years/",
+		"https://bmxfeed.com",
+		"https://bmxfeed.com/video",
+		"https://jsnmrs.github.io/pdfcheck/",
+		"https://jsnmrs.github.io/wordup/",
+		"https://jsnmrs.github.io/since/",
+		"https://doublegreat.dev",
+		"https://doublegreat.dev/theme/",
+		"https://doublegreat.dev/listen/",
+		"https://doublegreat.dev/listen/punctuation-in-alt-text/"
+	]
+};
